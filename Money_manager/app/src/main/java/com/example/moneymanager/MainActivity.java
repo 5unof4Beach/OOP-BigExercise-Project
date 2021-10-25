1f71693acb0bcbd19d6ed6046b0d46f534ff1345
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-10-25 10:58:34 +0700

    25/10/2021

--- Money_manager/app/src/main/java/com/example/moneymanager/MainActivity.java
+++ Money_manager/app/src/main/java/com/example/moneymanager/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.moneymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button expenseButton = (Button)findViewById(R.id.button_enter_expense);
+        Button incomeButton = (Button)findViewById(R.id.button_enter_income);
+        expenseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(Intent.ACTION_VIEW, /*ExpenseScreen.class*/Uri.parse("https://www.youtube.com/"));
+                Intent intent = new Intent(MainActivity.this, ExpenseScreen.class);
+                startActivity(intent);
+            }
+        });
+        incomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, IncomeScreen.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
