ed84b0493d87c1a3586fd8cdb6ea329801f5c2ae
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-10-27 21:01:32 +0700

    update

--- Money_manager/app/src/main/java/com/example/moneymanager/ExpenseFragmnet.java
+++ Money_manager/app/src/main/java/com/example/moneymanager/ExpenseFragmnet.java
@@ -7,11 +7,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class ExpenseFragmnet extends Fragment {
+    private ListView listView;
+    private String items[] = {"Clothes", "Houseware", "Education", "Computer", "Dog"};
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         return inflater.inflate(R.layout.fragment_expense, container, false);
     }
+
+//    @Override
+//    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+//        listView = (ListView) getView().findViewById(R.id.lv_category_list);
+//        CategoryAdapter adapter = new CategoryAdapter(this, items);
+//        listView.setAdapter(adapter);
+//    }
 }
