ea23a92d1cf71a305ac42a9b0890951d0e627a12
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-11-03 17:43:41 +0700

    edit some codes

--- Money_manager/app/src/main/java/com/example/moneymanager/IncomeFragment.java
+++ Money_manager/app/src/main/java/com/example/moneymanager/IncomeFragment.java
@@ -2,8 +2,6 @@ package com.example.moneymanager;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -19,12 +17,7 @@ import android.widget.Toast;
 import com.example.moneymanager.data.InputDataBaseHelper;
 import com.example.moneymanager.mainprocess.Input;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.util.Date;
-import java.util.Vector;
 
 public class IncomeFragment extends Fragment {
 
@@ -58,20 +51,17 @@ public class IncomeFragment extends Fragment {
     }
 
     public void enter(Button enterButton, EditText amountField){
-        enterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                clearAllFocus(amountField);
-                if(amount != 0){
-                    String show = amount + " " + " " + category + " " + date + "/" + month + "/" + year;
-                    addToDB();
-                    reInit(amountField);
-                    Toast.makeText(IncomeFragment.super.getContext(), show, Toast.LENGTH_SHORT).show();
+        enterButton.setOnClickListener(view -> {
+            clearAllFocus(amountField);
+            if(amount != 0){
+                String show = amount + " " + " " + category + " " + date + "/" + month + "/" + year;
+                addToDB();
+                reInit(amountField);
+                Toast.makeText(IncomeFragment.super.getContext(), show, Toast.LENGTH_SHORT).show();
 //                    Toast.makeText(ExpenseFragmnet.super.getContext(), "New Expense Added", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Toast.makeText(IncomeFragment.super.getContext(), "Please Enter Amount", Toast.LENGTH_SHORT).show();
-                }
+            }
+            else{
+                Toast.makeText(IncomeFragment.super.getContext(), "Please Enter Amount", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -89,16 +79,13 @@ public class IncomeFragment extends Fragment {
         amount = 0;
     }
     public void getAmount(EditText amountField){
-        amountField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                String temp = amountField.getText().toString();
-                if(!temp.equals("")){
-                    amount = Integer.parseInt(temp);
-                    Log.v("amount","added");
-                }
-
+        amountField.setOnFocusChangeListener((view, b) -> {
+            String temp = amountField.getText().toString();
+            if(!temp.equals("")){
+                amount = Integer.parseInt(temp);
+                Log.v("amount","added");
             }
+
         });
     }
 
@@ -115,28 +102,11 @@ public class IncomeFragment extends Fragment {
         int id = categoryField.getCheckedRadioButtonId();
         RadioButton button = (RadioButton) getView().findViewById(id);
         category = button.getText().toString();
-        categoryField.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                int id = categoryField.getCheckedRadioButtonId();
-                RadioButton button = (RadioButton) getView().findViewById(id);
-                category = button.getText().toString();
-            }
+        categoryField.setOnCheckedChangeListener((radioGroup, i) -> {
+            int id1 = categoryField.getCheckedRadioButtonId();
+            RadioButton button1 = (RadioButton) getView().findViewById(id1);
+            category = button1.getText().toString();
         });
     }
 
-//    protected void replaceFragmentContent(Fragment fragment) {
-//
-//        if (fragment != null) {
-//
-//            FragmentManager fmgr = getChildFragmentManager();
-//
-//            FragmentTransaction ft = fmgr.beginTransaction();
-//
-//            ft.replace(R.id.ll_income_fragment_layout, fragment);
-//
-//            ft.commit();
-//
-//        }
-//    }
 }
